be79e4bc07fab2a0d879bd2ad116847cc30f8e3f
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-09-27 10:39:11 -0300

    PropType and defaultProp

--- src/components/CardProduto.js
+++ src/components/CardProduto.js
@@ -1,18 +1,35 @@
+import PropType from 'prop-types';
 import stylesProduto from "../components/CardProduto.css";
 function CardProdutoPadaria({nomeDoProduto, preco, urlImagemProduto, descricaoImagem}) {
     return (
-        <div className={stylesProduto}>
-            <div class="card">
-                <img src={urlImagemProduto}
-                    alt={descricaoImagem}
-                    height="200px"
-                    className="imagem-produto"
-                />
-                <h3>{nomeDoProduto}</h3>
-                <h3>{preco}</h3>
+        <>
+            <div className={stylesProduto}>
+                <div class="card">
+                    <img src={urlImagemProduto}
+                        alt={descricaoImagem}
+                        height="200px"
+                        className="imagem-produto"
+                    />
+                    <h3>{nomeDoProduto}</h3>
+                    <h3>{preco}</h3>
+                </div>
             </div>
-        </div>
+        </>
     );
+}
+
+CardProdutoPadaria.prototype = {
+    nomeDoProduto: PropType.string,
+    preco: PropType.number,
+    urlImagemProduto: PropType.string,
+    descricaoImagem: PropType.string,
+}
+
+CardProdutoPadaria.defaultProps = {
+    nomeDoProduto: "Faltou o nome do produto",
+    preco: 0.0,
+    urlImagemProduto: "Faltou a URL da imagem",
+    descricaoImagem: "Faltou a descrição da imagem",
 }
 
 export default CardProdutoPadaria;
